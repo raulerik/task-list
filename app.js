751d73d33f9d90a558f87ae1f6af84c2bcb414ba
raulerik <raul.erik.laur@gmail.com>
2022-10-20 14:00:40 +0300

    Tasklist / added delete all function

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const form = document.querySelector('form');
 const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('#task-list');
+const deleteTasks = document.querySelector('#delete-tasks');
 
+taskList.addEventListener('click', deleteTask);
 form.addEventListener('submit', addTask);
+deleteTasks.addEventListener('click', deleteAllTasks)
 
 function addTask(e){
     // input value
@@ -20,8 +24,35 @@ function addTask(e){
     ul.appendChild(li);
     // clear input value
     taskInput.value = '';
+
+    // delete button
+    const x = document.createElement("a");
+    x.appendChild(document.createTextNode("X"));
+    x.setAttribute('href', "#");
+    x.className = 'secondary-content';
+    li.appendChild(x);
+
+    // delete input value from form input field
+    taskInput.value = ""
     // form submit event control
     e.preventDefault();
-    ;
 }
+
+function deleteTask(e) {
+    if(e.target.textContent === 'X') {
+        if(confirm('Are you sure to delete this task?')) {
+            e.target.parentElement.remove()
+        }
+    }
+
+
+}
+
+function deleteAllTasks(e) {
+    // taskList.innerHTML = ''
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
 
